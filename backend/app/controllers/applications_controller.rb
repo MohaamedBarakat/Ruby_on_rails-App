c9d14ddf19f90b74a6ec31db9ae255862d1a83d0
mohaamedbarakat <muhammad.barakat70@gmail.com>
2022-04-19 01:54:57 +0200

    add controller functions for Application model for all resorces index create update show

--- backend/app/controllers/applications_controller.rb
+++ backend/app/controllers/applications_controller.rb
@@ -1,23 +1,68 @@
 class ApplicationsController < ApplicationController
   def index
-    render json: {message: "hello"}
+    applications_data = Application.order(created_at: :asc)
+    render json: {message: "Applications Retrived", data:applications_data } , status: :ok
   end
 
   def create
+    begin
+      generated_token = generate_token
+
+      application = Application.new token: generated_token, name: params[:name]
+      application.save
+
+      no_of_application = Application.count
+
+      render json: {message: "Application Created", number_of_applications: no_of_application } , status: :created
+    rescue Exception => ex
+      render json: {error:ex ,message: "Cannot Create Application"} , status: :unprocessable_entity
+
+    end
   end
 
   def show
+    begin
+      application = Application.where token: params[:token]
+      is_valid_token(application)
+
+      render json: {message: "Application Retrived",application: application } , status: :ok
+    rescue Exception => ex
+      render json: {error: ex ,message: "Cannot fetch Application"} , status: :unprocessable_entity
+    end
+
   end
 
   def update
-  end
-  private
+    begin
+      application = Application.find_by token: params[:token]
 
+      application.name = params[:name]
+      application.save
+
+      render json: {message: "Application Updated",application: application } , status: :ok
+    rescue Exception => ex
+      render json: {error: ex ,message: "Cannot update Application"} , status: :unprocessable_entity
+    end
+  end
+
+  private
   def generate_token
     token = loop do
       random_token = SecureRandom.urlsafe_base64(32, false)
-      break random_token unless Book.exists?(token: random_token)
+      break random_token unless Application.exists?(token: random_token)
     end
   end
   
+  private
+  def application_params
+    params.require(:application).permit(:name)
+  end
+
+  private
+  def is_valid_token(application)
+    if application.count < 1
+      raise "token did not identfiy any application"
+    end  
+  end
+
 end
