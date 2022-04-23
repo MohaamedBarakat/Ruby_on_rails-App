ded089237ad306c92258a01aa1aa12e696ce9bdc
mohaamedbarakat <muhammad.barakat70@gmail.com>
2022-04-23 21:18:14 +0200

    justify docker-compose for last update

--- backend/app/models/message.rb
+++ backend/app/models/message.rb
@@ -2,8 +2,7 @@ require 'elasticsearch/model'
 
 class Message < ApplicationRecord
     belongs_to :chats, optional: true
-
-    #include Elasticsearch::Model
-    #include Elasticsearch::Model::Callbacks
+    include Elasticsearch::Model
+    include Elasticsearch::Model::Callbacks
 end
 
